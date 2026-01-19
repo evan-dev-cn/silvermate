c949bfda3811f84187f504de438d00ee533f1e76
丁翊峰 <dyf6688@gmail.com>
2026-01-19 21:38:20 +0800

    后端：初始化 Spring Boot 项目

---
+++ silvermate-backend/src/main/java/com/silvermate/silvermatebackend/SilvermateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.silvermate.silvermatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilvermateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilvermateBackendApplication.class, args);
+    }
+
+}
---
+++ silvermate-backend/src/test/java/com/silvermate/silvermatebackend/SilvermateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silvermate.silvermatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilvermateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
