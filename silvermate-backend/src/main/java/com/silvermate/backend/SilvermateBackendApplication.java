756f1fd7a8a2dfdc4cc298583931af6df50bf6e9
丁翊峰 <dyf6688@gmail.com>
2026-01-23 10:16:50 +0800

    后端结构

--- silvermate-backend/src/main/java/com/silvermate/backend/SilvermateBackendApplication.java
+++ silvermate-backend/src/main/java/com/silvermate/backend/SilvermateBackendApplication.java
@@ -1,4 +1,4 @@
-package com.silvermate.silvermatebackend;
+package com.silvermate.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- silvermate-backend/src/test/java/com/silvermate/backend/SilvermateBackendApplicationTests.java
+++ silvermate-backend/src/test/java/com/silvermate/backend/SilvermateBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.silvermate.silvermatebackend;
+package com.silvermate.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
